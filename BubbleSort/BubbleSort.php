bb5a822576f57c8bdd6c145d7cc9a91e8c87d471
Daniel Havelda <8553631dev@gmail.com>
2022-01-30 19:28:01 +0100

    Implemented Bubble sort

---
+++ BubbleSort/BubbleSort.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "./AlgorithmsAndDataStructures/SortingAlgorithm.php";
+include_once "./AlgorithmsAndDataStructures/SortingAlgorithmInterface.php";
+
+class BubbleSort extends SortingAlgorithm implements SortingAlgorithmInterface {
+  public function __construct(array $input)
+  {
+    parent::__construct($input);
+  }
+
+  public function sort(): array
+  {
+    $sortableArray = $this->getSortableArray();
+    $sortableArrLength = count($sortableArray);
+    
+    for ($i = 0; $i < $sortableArrLength - 1; $i++) {
+      // Az utolsó elem minden ciklus végén a megfelelő pozicióba kerül,
+      // ezért minden ciklussal egyre fogy a rendezetlen elemek száma (sortableArrLength - $i - 1) 
+      for ($j = 0; $j < ($sortableArrLength - $i) - 1; $j++) {
+        if ($sortableArray[$j] > $sortableArray[$j + 1]) {
+          $temp = $sortableArray[$j];
+          $sortableArray[$j] = $sortableArray[$j + 1];
+          $sortableArray[$j + 1] = $temp;
+        }
+      }
+    }
+
+    $this->setSortedArray($sortableArray);
+    return $this->getSortedArray();
+  }
+}
---
+++ BubbleSort/README.md
@@ -0,0 +1,8 @@
+# Bubble Sort
+
+## Mi ez?
+Egy olyan rendezési algoritmus, amely ismételten végigmegy az adott listán, összehasonlítja
+a szomszédos elemeket, és felcseréli őket, ha rossz sorrendben vannak. A lista átjárása
+annyiszor kezdődik újra, míg a lista rendezett nem lesz. Az ismétlődő bejárások során
+a vizsgált elemek száma csökken, mivel a lista végén lévő elemek már az előző
+bejárások során rendezve lettek.
